f4c5dbf2538daaa1ec39ed1fce10b14a598cee74
danish <s.danish0827@gmail.com>
2025-08-24 13:19:09 +0530

    adding github action add nextjs file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true, // recommended for static hosting
+  trailingSlash: false,
 };
 
 module.exports = nextConfig;
